5d09fdd4bc2395a5132ba8d204d948311737cec6
Cris Trinidad <trinidadcristobal15@gmail.com>
2024-10-02 20:11:18 -0700

    -Changed welcome page

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,7 +5,6 @@ function Hello() {
     <>
       <div className="container right-welcome-container">
         <h4 className="display-4">Hi! I'm Cris Trinidad.</h4>
-        <h6 className="display-6">Aspiring Software Developer</h6>
         <p className="lead">
           Currently a QA Analyst at Unison Global. <br /> My hobbies include
           knitting, cooking, and hiking.
